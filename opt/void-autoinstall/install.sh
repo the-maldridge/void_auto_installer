fd42ab8f5b18ca862c90e03b881d54d900e4faa3
Michael Aldridge <aldridge.mac@gmail.com>
2016-03-22 13:38:06 -0500

    Functions Everywhere!

--- opt/void-autoinstall/install.sh
+++ opt/void-autoinstall/install.sh
@@ -1,48 +1,38 @@
 #!/bin/bash
 
-# Get an IP address
-dhcpcd -w
-sleep 15
+# These functions pulled from void's excellent mklive.sh
 
-bootpartitionsize="500M"
-disk=`lsblk -ipo NAME,TYPE,MOUNTPOINT | awk '{if ($2=="disk") {disks[$1]=0; last=$1} if ($3=="/") {disks[last]++}} END {for (a in disks) {if(disks[a] == 0){print a; break}}}'`
-swapsize="`grep MemTotal /proc/meminfo | awk '{print $2}'`K";
-mountpoint="/mnt"
-xbpsrepository="http://lug.utdallas.edu/mirror/void/current"
-timezone="America/Chicago"
-keymap="us"
-libclocale="en_US.UTF-8"
-username="cv"
 
-procurl=`awk '{for(i = 1; i <= NF; i++) { split($i,a,"="); if(a[1]=="autourl"){print a[2]} }}' /proc/cmdline`
+info_msg() {
+    printf "\033[1m$@\n\033[m"
+}
+die() {
+    info_msg "ERROR: $@"
+    exit 1
+}
+print_step() {
+    CURRENT_STEP=$((CURRENT_STEP+1))
+    info_msg "[${CURRENT_STEP}/${STEP_COUNT}] $@"
+}
 
-if [ -z "$procurl" ]
-then
-        echo "No config url found in /proc/cmdline."
-else
-        xbps-install -Sy curl
-        curl -o config.cfg $procurl
-fi
+# ----------------------- Install Functions ------------------------
 
-while getopts "u:" opt; do
-        case $opt in
-                u)
-                        xbps-install -Sy curl
-                        curl -o config.cfg $OPTARG
-                        ;;
-                \?)
-                        echo "Invalid option: -$OPTARG" >&2
-                        exit 1
-                        ;;
-        esac
+welcome() {
+    clear
+    printf "============================================================="
+    printf "================ Void Linux Auto-Installer =================="
+    printf "============================================================="
+}
 
-done
+get_address() {
+    # Get an IP address
+    dhcpcd -w
+    sleep 15
+}
 
-[ -f ./config.cfg ] && echo "Reading configuration file" && source ./config.cfg
-[ -z "$disk" ] && echo "No disk found." && exit 1
-
-# Paritition Disk
-fdisk $disk <<EOF
+partition_disk() {
+    # Paritition Disk
+    fdisk $disk <<EOF
 o
 n
 p
@@ -65,27 +55,34 @@ p
 w
 q
 EOF
+}
 
-# Make Filesystems
-mkfs.ext4 -F "${disk}1"
-mkfs.ext4 -F "${disk}3"
-mkswap -f "${disk}2"
+format_disk() {
+    # Make Filesystems
+    mkfs.ext4 -F "${disk}1"
+    mkfs.ext4 -F "${disk}3"
+    mkswap -f "${disk}2"
+}
 
-# Mount our chroot
-mount "${disk}3" $mountpoint
-mkdir "${mountpoint}/boot"
-mount "${disk}1" "${mountpoint}/boot"
+mount_target() {
+    # Mount targetfs
+    mount "${disk}3" $mountpoint
+    mkdir "${mountpoint}/boot"
+    mount "${disk}1" "${mountpoint}/boot"
+}
 
+install_base_system() {
+    # Install a base system
+    xbps-install -Sy -R $xbpsrepository -r /mnt base-system grub ed $pkgs
+}
 
-# Install a base system
-xbps-install -Sy -R $xbpsrepository -r /mnt base-system grub ed $pkgs
-
-# Write the *real* install script
-xbps-install -Sy bind-utils ed
-ip_address=`ip a | grep 'inet' | grep -v ' lo' | awk '{print $2}' | sed 's/\/.*$//'`
-hostname=`dig +short -x $ip_address | sed 's/\..*$//'`
-[ -z "$hostname" ] && hostname="void-computer"
-ed -s ./chroot_install.sh <<EOF
+template_chroot_script() {
+    # Write the *real* install script
+    xbps-install -Sy bind-utils ed
+    ip_address=`ip a | grep 'inet' | grep -v ' lo' | awk '{print $2}' | sed 's/\/.*$//'`
+    hostname=`dig +short -x $ip_address | sed 's/\..*$//'`
+    [ -z "$hostname" ] && hostname="void-computer"
+    ed -s ./chroot_install.sh <<EOF
 ,s_%HOSTNAME%_${hostname}_g
 ,s,%DISK%,$disk,g
 ,s,%TIMEZONE%,$timezone,g
@@ -94,18 +91,109 @@ ed -s ./chroot_install.sh <<EOF
 ,s/%USERNAME%/$username/g
 w
 EOF
+}
 
-# Copy file and make sure it is executable
-cp ./chroot_install.sh $mountpoint
-chmod +x "${mountpoint}/chroot_install.sh"
+install_chroot_script() {
+    # Copy file and make sure it is executable
+    cp ./chroot_install.sh $mountpoint
+    chmod +x "${mountpoint}/chroot_install.sh"
+}
 
-# Mount dev, bind, proc, etc into chroot
-mount -t proc proc "${mountpoint}/proc"
-mount -t sysfs sys "${mountpoint}/sys"
-mount -o bind /dev "${mountpoint}/dev"
-mount -t devpts pts "${mountpoint}/dev/pts"
+prepare_chroot() {
+    # Mount dev, bind, proc, etc into chroot
+    mount -t proc proc "${mountpoint}/proc"
+    mount -t sysfs sys "${mountpoint}/sys"
+    mount -o bind /dev "${mountpoint}/dev"
+    mount -t devpts pts "${mountpoint}/dev/pts"
+}
 
-cd $mountpoint
-# Run the install tasks
-chroot $mountpoint "./chroot_install.sh"
-#reboot
+run_chroot_script() {
+    # Run the install tasks
+    cd $mountpoint
+    chroot $mountpoint "./chroot_install.sh"
+}
+
+
+configure_autoinstall() {
+    # this has to happen after the network since we might need curl
+
+    # -------------------------- Setup defaults ---------------------------
+    bootpartitionsize="500M"
+    disk=`lsblk -ipo NAME,TYPE,MOUNTPOINT | awk '{if ($2=="disk") {disks[$1]=0; last=$1} if ($3=="/") {disks[last]++}} END {for (a in disks) {if(disks[a] == 0){print a; break}}}'`
+    swapsize="`grep MemTotal /proc/meminfo | awk '{print $2}'`K";
+    mountpoint="/mnt"
+    xbpsrepository="http://lug.utdallas.edu/mirror/void/current"
+    timezone="America/Chicago"
+    keymap="us"
+    libclocale="en_US.UTF-8"
+    username="voidlinux"
+
+    # --------------- Pull config URL out of kernel cmdline -------------------------
+    procurl=`awk '{for(i = 1; i <= NF; i++) { split($i,a,"="); if(a[1]=="autourl"){print a[2]} }}' /proc/cmdline`
+    if [ -z "$procurl" ]
+    then
+	info_msg "No config url found in /proc/cmdline."
+    else
+	xbps-install -Sy curl
+	curl -o config.cfg $procurl
+    fi
+
+    # ---------------- Parse command line args ----------------------------
+    while getopts "u:" opt; do
+	case $opt in
+	    u)
+		xbps-install -Sy curl
+		curl -o config.cfg $OPTARG
+		;;
+	    \?)
+		die "Invalid option: -$OPTARG" >&2
+		exit 1
+		;;
+	esac
+
+    done
+
+    # Read in the resulting config file which we got via some method
+    [ -f ./config.cfg ] && info_msg "Reading configuration file" && source ./config.cfg
+
+    # Bail out if we didn't get a usable disk
+    [ -z "$disk" ] && die "No valid disk!"
+}
+
+# ------------------- Install "main()" ----------------------------
+
+CURRENT_STEP=0
+STEP_COUNT=9
+
+
+welcome
+
+print_step "Bring up the network"
+get_address
+
+print_step "Configure installer"
+configure_autoinstall
+
+print_step "Configuring disk using scheme 'Atomic'"
+partition_disk
+format_disk
+
+print_step "Mounting the target filesystems"
+mount_target
+
+print_step "Installing the base system"
+install_base_system
+
+print_step "Configuring chroot installer"
+template_chroot_script
+
+print_step "Installing chroot installer"
+install_chroot_script
+
+print_step "Prepare the chroot"
+prepare_chroot
+
+print_step "Executing chroot installer"
+run_chroot_script
+
+info_msg "Installation Complete!"
