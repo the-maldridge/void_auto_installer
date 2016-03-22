426683b9322f7cd6955957705cee4c00c1c5be13
Michael Aldridge <aldridge.mac@gmail.com>
2016-03-22 15:48:28 -0500

    User functions in the chroot install

--- opt/void-autoinstall/chroot_install.sh
+++ opt/void-autoinstall/chroot_install.sh
@@ -1,20 +1,46 @@
 #!/bin/sh
 
-# Fix permissions
-chown root:root /
-chmod 755 /
+# These functions pulled from void's excellent mklive.sh
 
-useradd -m -s /bin/bash -U -G wheel,users,audio,video,cdrom,input %USERNAME%
-passwd %USERNAME%
 
-# Give wheel sudo
-echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+info_msg() {
+    printf "\033[1min-target: $@\n\033[m"
+}
+die() {
+    info_msg "ERROR: $@"
+    exit 1
+}
+print_step() {
+    CURRENT_STEP=$((CURRENT_STEP+1))
+    info_msg "[${CURRENT_STEP}/${STEP_COUNT}] $@"
+}
 
-# Set the hostname
-echo "%HOSTNAME%" > /etc/hostname
+# ----------------------- Install Functions ------------------------
 
-# Fix the rc.conf file
-ed -s /etc/rc.conf <<EOF
+correct_permission() {
+    # Fix permissions
+    chown root:root /
+    chmod 755 /
+}
+
+add_user() {
+    useradd -m -s /bin/bash -U -G wheel,users,audio,video,cdrom,input %USERNAME%
+    passwd %USERNAME%
+}
+
+grant_sudo() {
+    # Give wheel sudo
+    echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+}
+
+set_hostname() {
+    # Set the hostname
+    echo "%HOSTNAME%" > /etc/hostname
+}
+
+configure_rc_conf() {
+    # Fix the rc.conf file
+    ed -s /etc/rc.conf <<EOF
 ,s_void-live_%HOSTNAME%_
 ,s,Europe/Madrid,%TIMEZONE%,
 ,s/"es"/"%KEYMAP%"
@@ -24,14 +50,16 @@ ed -s /etc/rc.conf <<EOF
 /KEYMAP/s/#//
 w
 EOF
+}
 
-# Grab UUIDS
-uuid1=`blkid | grep '%DISK%1:' | awk -F '"' '{print $2}'`
-uuid2=`blkid | grep '%DISK%2:' | awk -F '"' '{print $2}'`
-uuid3=`blkid | grep '%DISK%3:' | awk -F '"' '{print $2}'`
+configure_fstab() {
+    # Grab UUIDS
+    uuid1=`blkid | grep '%DISK%1:' | awk -F '"' '{print $2}'`
+    uuid2=`blkid | grep '%DISK%2:' | awk -F '"' '{print $2}'`
+    uuid3=`blkid | grep '%DISK%3:' | awk -F '"' '{print $2}'`
 
-# Fix the fstab file
-ed -s /etc/fstab <<EOF
+    # Fix the fstab file
+    ed -s /etc/fstab <<EOF
 /tmpfs/d
 a
 UUID=$uuid3 / ext4 defaults,errors=remount-ro 0 1
@@ -40,22 +68,55 @@ UUID=$uuid2 swap swap defaults 0 0
 .
 w
 EOF
+}
 
-# Set the libc-locale
-ed -s /etc/default/libc-locales <<EOF
+configure_locale() {
+    # Set the libc-locale
+    ed -s /etc/default/libc-locales <<EOF
 /%LIBCLOCALE%/s/#//
 w
 EOF
 
-xbps-reconfigure -f glibc-locales
+    xbps-reconfigure -f glibc-locales
+}
 
-# Set hostonly
-echo "hostonly=yes" > /etc/dracut.conf.d/hostonly.conf
+configure_grub() {
+    # Set hostonly
+    echo "hostonly=yes" > /etc/dracut.conf.d/hostonly.conf
 
-# Choose the newest kernel
-kernel_version=`ls /lib/modules | cut -f1,2 -d'.' | uniq | sort -Vr | sed -n 1p`
+    # Choose the newest kernel
+    kernel_version=`ls /lib/modules | cut -f1,2 -d'.' | uniq | sort -Vr | sed -n 1p`
 
-# Install grub
-grub-install %DISK%
-xbps-reconfigure -f "linux${kernel_version}"
-passwd -l root
+    # Install grub
+    grub-install %DISK%
+    xbps-reconfigure -f "linux${kernel_version}"
+    passwd -l root
+}
+
+# -------------- chroot "main()" ---------------------------
+CURRENT_STEP=0
+STEP_COUNT=8
+
+print_step "Correcting permissions on /"
+correct_permission
+
+print_step "Adding initial user %USERNAME%"
+add_user
+
+print_step "Granting sudo to initial user: %USERNAME%"
+grant_sudo
+
+print_step "Setting hostname to %HOSTNAME%"
+set_hostname
+
+print_step "Configuring /etc/rc.conf"
+configure_rc_conf
+
+print_step "Configuring mounts in /etc/fstab"
+configure_fstab
+
+print_step "Configuring glib-locale"
+configure_locale
+
+print_step "Configuring and installing grub"
+configure_grub
